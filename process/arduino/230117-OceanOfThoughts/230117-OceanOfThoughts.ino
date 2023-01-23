8b326c9370fe5e08968635efccfc2c2b6b4dc5f0
Amaury <amaury.hamon@gmail.com>
2023-01-23 11:59:44 +0100

    update

--- process/arduino/230117-OceanOfThoughts/230117-OceanOfThoughts.ino
+++ process/arduino/230117-OceanOfThoughts/230117-OceanOfThoughts.ino
@@ -1,4 +1,4 @@
-    #include <FastLED.h>
+#include <FastLED.h>
 #include <Wire.h>
 #include <mpr121.h>
 
@@ -28,72 +28,74 @@ uint8_t colorIndex[NUM_LEDS];
 // converted for FastLED with gammas (2.6, 2.2, 2.5)
 // Size: 28 bytes of program space.
 
-DEFINE_GRADIENT_PALETTE( es_pinksplash_07_gp ) {
-    0, 229,  1,  1,
-   61, 242,  4, 63,
-  101, 255, 12,255,
-  127, 249, 81,252,
-  153, 255, 11,235,
-  193, 244,  5, 68,
-  255, 232,  1,  5};
+DEFINE_GRADIENT_PALETTE(es_pinksplash_07_gp){
+  0, 229, 1, 1,
+  61, 242, 4, 63,
+  101, 255, 12, 255,
+  127, 249, 81, 252,
+  153, 255, 11, 235,
+  193, 244, 5, 68,
+  255, 232, 1, 5
+};
 
-DEFINE_GRADIENT_PALETTE( algae_gp ) {
-    0, 163,241,153,
-    1, 161,239,149,
-    3, 155,235,144,
-    4, 153,233,142,
-    5, 152,231,138,
-    6, 148,231,137,
-    7, 146,229,133,
-    8, 144,227,132,
-    9, 142,225,130,
-   10, 139,223,127,
-   11, 137,221,125,
-   12, 135,219,122,
-   13, 133,217,120,
-   14, 130,215,117,
-   15, 128,213,115,
-   16, 126,213,114
-  };
+DEFINE_GRADIENT_PALETTE(algae_gp){
+  0, 163, 241, 153,
+  1, 161, 239, 149,
+  3, 155, 235, 144,
+  4, 153, 233, 142,
+  5, 152, 231, 138,
+  6, 148, 231, 137,
+  7, 146, 229, 133,
+  8, 144, 227, 132,
+  9, 142, 225, 130,
+  10, 139, 223, 127,
+  11, 137, 221, 125,
+  12, 135, 219, 122,
+  13, 133, 217, 120,
+  14, 130, 215, 117,
+  15, 128, 213, 115,
+  16, 126, 213, 114
+};
 
-DEFINE_GRADIENT_PALETTE( rain_gp ) {
-    0, 213,217,226,
-    1, 213,215,221,
-    2, 210,213,219,
-    3, 208,211,214,
-    4, 206,209,210,
-    5, 206,207,207,
-    6, 203,205,203,
-    7, 199,201,197,
-    8, 199,201,197,
-    9, 197,197,190,
-   10, 194,195,188,
-   11, 192,193,184,
-   12, 190,189,182,
-   13, 190,187,178,
-   14, 188,186,176,
-   15, 188,186,176,
-   16, 188,184,172};
+DEFINE_GRADIENT_PALETTE(rain_gp){
+  0, 213, 217, 226,
+  1, 213, 215, 221,
+  2, 210, 213, 219,
+  3, 208, 211, 214,
+  4, 206, 209, 210,
+  5, 206, 207, 207,
+  6, 203, 205, 203,
+  7, 199, 201, 197,
+  8, 199, 201, 197,
+  9, 197, 197, 190,
+  10, 194, 195, 188,
+  11, 192, 193, 184,
+  12, 190, 189, 182,
+  13, 190, 187, 178,
+  14, 188, 186, 176,
+  15, 188, 186, 176,
+  16, 188, 184, 172
+};
 
 
-DEFINE_GRADIENT_PALETTE( Caribbean_gp ) {
-    0,   0,  0, 43,
-   24,   0,  5, 85,
-   36,   0, 10,106,
-   48,   1, 22,130,
-   60,   1, 32,147,
-   84,   1, 79,138,
-   97,   1, 91,147,
-  109,   3,104,156,
-  121,  12,104,166,
-  145,  35,118,176,
-  157,  64,118,176,
-  237, 184,125, 26,
-  237, 184,125, 27,
-  240, 206,141, 25,
-  242, 229,149, 25,
-  245, 242,166, 24
-  };
+DEFINE_GRADIENT_PALETTE(Caribbean_gp){
+  0, 0, 0, 43,
+  24, 0, 5, 85,
+  36, 0, 10, 106,
+  48, 1, 22, 130,
+  60, 1, 32, 147,
+  84, 1, 79, 138,
+  97, 1, 91, 147,
+  109, 3, 104, 156,
+  121, 12, 104, 166,
+  145, 35, 118, 176,
+  157, 64, 118, 176,
+  237, 184, 125, 26,
+  237, 184, 125, 27,
+  240, 206, 141, 25,
+  242, 229, 149, 25,
+  245, 242, 166, 24
+};
 
 CRGBPalette16 currentPalette(es_pinksplash_07_gp);
 CRGBPalette16 targetPalette(Caribbean_gp);
@@ -106,7 +108,7 @@ CRGBPalette16 rainPal(rain_gp);
 
 int key = 0;
 int journeyStep = 0;
-uint8_t whichPalette=0;
+uint8_t whichPalette = 0;
 
 bool touchHold = true;
 
@@ -117,8 +119,8 @@ void setup() {
   Wire.begin();
   CapaTouch.begin();
 
-  //EXTRA RANDOMNESS!!!!! 
-  randomSeed(analogRead(0));  
+  //EXTRA RANDOMNESS!!!!!
+  randomSeed(analogRead(0));
 
   //Setup LEDs
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
@@ -131,28 +133,28 @@ void setup() {
 
   //For blendPalette setup
   //fill the colorIndex array with random numbers
-  for(int i=0;i<NUM_LEDS;i++){
-    colorIndex[i]= random8();
+  for (int i = 0; i < NUM_LEDS; i++) {
+    colorIndex[i] = random8();
   }
 
   FastLED.show();
 }
 void loop() {
   changeBrightness();  //Setup adjustable brightness
-  touchControls();  //Setup controls on wheelpad Capacitive Touch
-  
-  
+  touchControls();     //Setup controls on wheelpad Capacitive Touch
+
+
   FastLED.show();
   fadeToBlackBy(leds, NUM_LEDS, 10);
-  // Serial.println(whichPalette);
+   Serial.println(whichPalette);
 }
 void changeBrightness() {
   //Adjust brightness with potentiometer
   int potentiometerValue = analogRead(POTENTIOMETER_PIN);
-  int brightness = constrain(map(potentiometerValue, 33, 1023, 0, 255),0,255);
+  int brightness = constrain(map(potentiometerValue, 33, 1023, 0, 255), 0, 255);
   // Serial.print(potentiometerValue);
   // Serial.print("\t");
-  // Serial.println(brightness); 
+  // Serial.println(brightness);
   FastLED.setBrightness(brightness);
 }
 
@@ -161,26 +163,55 @@ void touchControls() {
   key = CapaTouch.wheelKey();
   bool newTouch = true;
   // Helper to check which case goes for which direction
-   if (key >= 0) {
+  if (key >= 0) {
     // Serial.print("real key = ");
     Serial.println(key);
-  }
-  else {
+  } else {
     // Serial.println("-1");
   }
   switch (key) {
     case 9:  // North
       FastLED.clear();
-      if (!touchHold) {
-        drawRadius360(0, CRGB::Yellow, 0, 5);
-        whichPalette++; 
-      } 
-      // else {
-      //    blendPalette();
-      // }
-      
-      break;
+      drawRadius360(0, CRGB::Yellow, 0, 5);
 
+
+      if (!touchHold) {
+        blendPalette();
+        whichPalette++;
+      }
+      else {
+      }
+      break;
+    case 5:  // West
+      FastLED.clear();
+      // shipWheel1a();
+      drawRadius360(270, CRGB::Yellow, 0, 5);
+      //If this is not hold, then do stepLess
+      if (!touchHold) {
+        stepLess();
+      }
+      break;
+    case 13:  //  East
+      FastLED.clear();
+      // shipWheel1a();
+
+      if (!touchHold) {
+        stepMore();
+        drawRadius360(90, CRGB::Yellow, 0, 5);
+      }
+      break;
+    case 1:  // South
+      FastLED.clear();
+      blendPalette();
+      if (!touchHold) {
+        drawRadius360(180, CRGB::Yellow, 0, 5);
+        whichPalette--;
+      }
+      else {
+      }
+      
+
+      break;
     case 10:  // North North East
       FastLED.clear();
       // shipWheel1a();
@@ -202,19 +233,11 @@ void touchControls() {
       }
       break;
 
-    case 13:  //  East
-      FastLED.clear();
-      // shipWheel1a();
-      
-      if (!touchHold) {
-        stepMore();
-        drawRadius360(90, CRGB::Yellow, 0, 5);
-      }
-      break;
+    
     case 14:  //  East South East
       FastLED.clear();
       // shipWheel1a();
-      
+
       if (!touchHold) {
         stepMore();
         drawRadius360(90 + 22.5, CRGB::Yellow, 0, 5);
@@ -231,13 +254,6 @@ void touchControls() {
       FastLED.clear();
       // shipWheel1a();
       drawRadius360(90 + 45 + 22.5, CRGB::Yellow, 0, 5);
-      // delay(DELAY);
-      break;
-    case 1:  // South
-      FastLED.clear();
-      whichPalette--;
-      // shipWheel1a();
-      drawRadius360(180, CRGB::Yellow, 0, 5);
       // delay(DELAY);
       break;
     case 2:  // South South West
@@ -259,15 +275,6 @@ void touchControls() {
       if (!touchHold) {
         stepLess();
       }
-      break;
-    case 5:  // West
-      FastLED.clear();
-      // shipWheel1a();
-      drawRadius360(270, CRGB::Yellow, 0, 5);
-      //If this is not hold, then do stepLess
-      if (!touchHold) {
-        stepLess();
-      } 
       break;
     case 6:  // West North West
       FastLED.clear();
@@ -356,8 +363,7 @@ void strandTest() {
 void step0() {
   // pacifica_loop();
   EVERY_N_SECONDS(2) {
-      setPixel360(0, 0, CRGB::White);
-
+    setPixel360(0, 0, CRGB::White);
   }
   fadeToBlackBy(leds, NUM_LEDS, .2);
   // EVERY_N_MILLISECONDS(3000) {
@@ -367,17 +373,17 @@ void step0() {
   //   }
   // }
   // delay(10);
-
 }
 //CLEAR SKY SUNRISE
 void step1() {
 
   pacifica_loop();
-  for(int i=0;i<2;i++){
-    setPixel360(random(360), 1, CRGB::White);
-    break;
-  }
-  
+  setPixel360(0, 1, CRGB::White);
+  // for(int i=0;i<2;i++){
+
+  //   break;
+  // }
+
 
 
   // EVERY_N_MILLISECONDS(100) {
@@ -438,24 +444,11 @@ void step5() {
 }
 //STORM VORTEX RESET
 void step6() {
+  FastLED.clear();
   vortex();
-  
-  // EVERY_N_SECONDS(1) {
-  //   for (int i = 0; i < NUM_LEDS; i++) {
-  //     // Set the i'th led to red
-  //     static uint8_t hue = 0;
-  //     leds[i] = CHSV(hue++, 255, 255);
-
-  //     for (int i = 0; i <= journeyStep; i++) {
-  //       setPixel360(random(360), i, CRGB::White);
-  //       fadeToBlackBy(leds, NUM_LEDS, 50);
-  //     }
-
-  //     FastLED.show();
-  //     fadeToBlackBy(leds, NUM_LEDS, 20);
-  //     // delay(100);
-  //   }
-  // }
+  EVERY_N_MILLISECONDS(5000) {
+    lightning();
+  }
   fadeToBlackBy(leds, NUM_LEDS, 2);
 }
 //////////////////////////////////////////////////////////////
@@ -499,59 +492,60 @@ void lightning() {
   delay(random8(FREQUENCY) * 100);  // delay between strikes
 }
 
-void blendPalette(){
+void blendPalette() {
   //color each pixel from the palette using the index from colorIndex[]
-  for(int i=0;i<NUM_LEDS;i++){
-    leds[i]=ColorFromPalette(currentPalette, colorIndex[i]);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(currentPalette, colorIndex[i]);
   }
-  nblendPaletteTowardPalette(currentPalette, targetPalette, 5); //currentPalette to targetPalette, and speed
-  switch(whichPalette){
+  nblendPaletteTowardPalette(currentPalette, targetPalette, 5);  //currentPalette to targetPalette, and speed
+  switch (whichPalette) {
     case 0:
-      targetPalette=algae_gp;
+      targetPalette = algae_gp;
       break;
     case 1:
-      targetPalette=rain_gp;
+      targetPalette = rain_gp;
       break;
     case 2:
-      targetPalette=Caribbean_gp;
+      targetPalette = Caribbean_gp;
       break;
   }
-  if(whichPalette>3) whichPalette=0;
-  EVERY_N_SECONDS(5){
+  if (whichPalette > 3) whichPalette = 0;
+  EVERY_N_SECONDS(5) {
     // whichPalette++;
-    
   }
   FastLED.show();
 }
-void rainAnim(){ //sparkling effect
+void rainAnim() {  //sparkling effect
   // Switch on a LED at random, choosing from random color from palette
-  EVERY_N_MILLISECONDS(50){
-    leds[random8(0,NUM_LEDS-1)] = ColorFromPalette(rainPal, random8(),255,LINEARBLEND);
+  EVERY_N_MILLISECONDS(50) {
+    leds[random8(0, NUM_LEDS - 1)] = ColorFromPalette(rainPal, random8(), 255, LINEARBLEND);
   }
   //Fade all LEDS down by 1 in Brightness each time this is called
   // fadeToBlackBy(leds, NUM_LEDS, 1); //rain style
-  fadeToBlackBy(leds, NUM_LEDS, 10); //heavy rain style
+  fadeToBlackBy(leds, NUM_LEDS, 10);  //heavy rain style
   // fadeToBlackBy(leds, NUM_LEDS, 100); //electrical sparks style
-  
+
 
   FastLED.show();
 }
-void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
-void vortex(){
+void fadeall() {
+  for (int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); }
+}
+void vortex() {
   static uint8_t hue = 0;
-	Serial.print("x");
-	// First slide the led in one direction
+  Serial.print("x");
+  // First slide the led in one direction
 
-	for(int i = 0; i < NUM_LEDS; i++) {
-		// Set the i'th led to red 
-		leds[i] = CHSV(hue++, 81, 63); //removed hue++
-		// Show the leds
-		FastLED.show(); 
-		// now that we've shown the leds, reset the i'th led to black
-		// leds[i] = CRGB::Black;
-		fadeall();
-		// Wait a little bit before we loop around and do it again
-		delay(50);
-	}
-	Serial.print("x");
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // Set the i'th led to red
+    leds[i] = CHSV(hue++, 200, 63);  //removed hue++
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(50);
+  }
+  Serial.print("x");
 }
